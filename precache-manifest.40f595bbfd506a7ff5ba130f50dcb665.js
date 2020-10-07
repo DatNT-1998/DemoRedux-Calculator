999fbd4a72948ea7a8b353e3e004367a8ec51098
DatNT1998 <44127716+DatNT1998@users.noreply.github.com>
2020-10-07 14:31:34 +0700

    Updates

---
+++ precache-manifest.40f595bbfd506a7ff5ba130f50dcb665.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4456dd1424a481f4ebbb323c54b9ecb",
+    "url": "/DemoRedux-Calculator/index.html"
+  },
+  {
+    "revision": "f6ab6ce85494308ebedf",
+    "url": "/DemoRedux-Calculator/static/css/main.f7fa8be7.chunk.css"
+  },
+  {
+    "revision": "a593ea1d8367dd849960",
+    "url": "/DemoRedux-Calculator/static/js/2.b2a192fa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/DemoRedux-Calculator/static/js/2.b2a192fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6ab6ce85494308ebedf",
+    "url": "/DemoRedux-Calculator/static/js/main.fcd4e809.chunk.js"
+  },
+  {
+    "revision": "df544504510926da7990",
+    "url": "/DemoRedux-Calculator/static/js/runtime-main.75c16e72.js"
+  }
+]);
